49f5ac7110502238c652126c5f48a707f63d3be1
Tomasz Cetnar <tomekcet@gmail.com>
2017-07-16 20:35:23 +0200

    HelloWorld, Data Types

---
+++ src/kodzenie/HelloWorld.java
@@ -0,0 +1,14 @@
+package kodzenie;
+
+public class HelloWorld {
+
+	public	static void main(String args[]){
+		System.out.println("Hello World!");
+		returnString("Tomasz Cetnar");
+	}
+	
+	static void returnString(String string){
+		System.out.println(string);
+	}
+}
+
---
+++ src/kodzenie/TypyDanych.java
@@ -0,0 +1,16 @@
+package kodzenie;
+
+public class TypyDanych {
+
+	public static void main(String[] args) {
+		int wiek = 25;
+		boolean prawda = true;
+		String inicjaly = "J K";
+		System.out.println("- Witaj Jan Kowalski, s³ysza³em, ¿e masz "+wiek+ " lat, czy to prawda?" );
+		System.out.println("- "+prawda);
+		System.out.println("- Czy Twoje inicjaly to " + inicjaly + " ?");
+		System.out.println("- Tak, "+prawda);
+
+	}
+
+}
